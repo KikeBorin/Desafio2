697e1bc27c5102cb416c66580fb32a5fec9ccb10
Fernando Borin <fernando_borin@hotmail.com>
2022-05-06 21:45:49 -0300

    atulizacao final

--- src/Paginas/Favoritos.jsx
+++ src/Paginas/Favoritos.jsx
@@ -1,13 +1,23 @@
 import React from "react";
 
-function Favoritos(props){
+function Favoritos(props) {
     console.log(props.projetos)
 
-    return(
+    return (
         <>
             {props.projetos[0] !== undefined && (
                 props.projetos.map(projeto => (
-                    <h1>{projeto.name}</h1>
+                    <>
+                        <ol class="list-group mb-2">
+                            <li class="list-group-item d-flex justify-content-between align-items-start">
+                                <div class="ms-2 me-auto">
+                                    <div class="fw-bold">{projeto.name}</div>
+                                    {projeto.description}
+                                </div>
+                                <span class="badge bg-primary rounded-pill">{projeto.watchers}</span>
+                            </li>                           
+                        </ol>
+                    </>
                 ))
             )}
 
